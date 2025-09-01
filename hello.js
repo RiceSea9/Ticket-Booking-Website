94873fad57becb919ddeb8db3da16067c1ed44a3
Navjot Kaur <navjotkaur4250@gmail.com>
2025-09-01 13:58:26 +0530

    Initial commit

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+let age = 10;
+if(age>=18){
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote.");
+}
+let globalVar = "I am global";
+function test(){
+    let localVar = "I am local";
+    console.log
+}
